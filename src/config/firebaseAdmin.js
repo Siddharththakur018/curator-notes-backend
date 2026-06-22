7b878e136c240d19fb33b95532d0522d033e20f5
Siddharth Thakur <u803760@gmail.com>
2026-06-22 12:37:43 +0530

    fixes

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,9 +1,25 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const requiredEnv = [
+  "FIREBASE_PROJECT_ID",
+  "FIREBASE_CLIENT_EMAIL",
+  "FIREBASE_PRIVATE_KEY",
+];
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+for (const key of requiredEnv) {
+  if (!process.env[key]) {
+    throw new Error(`Missing Firebase environment variable: ${key}`);
+  }
+}
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
+}
 
 module.exports = admin;
