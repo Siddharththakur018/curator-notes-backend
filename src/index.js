73bae391964caf21b65808cb53ada01cbff06325
Siddharth Thakur <u803760@gmail.com>
2026-06-22 12:18:58 +0530

    fixes

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3001", "http://localhost:3000", "https://curator-notes-frontend.vercel.app/"],
+    origin: ["http://localhost:3001", "http://localhost:3000", "https://curator-notes-frontend.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
